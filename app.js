28e9479cba637e8344efdc73d5ff1dd74bc56cf8
Rory Manathunga <thedarkknight@Rorys-MacBook-Pro-2.local>
2019-07-29 23:03:43 -0700

    5:05 Yargs Part 2

--- app.js
+++ app.js
@@ -12,9 +12,55 @@ yargs.command({
 
   command:'add',
   describe:'Add a new note',
+  builder: {
+    title:{
+      describe:'Note Title',
+      demandOption: true,
+      type: 'string' 
+    }
+  },
+  handler: function (argv) {
+
+    console.log('Adding a new note', argv)
+
+  }
+
+})
+
+//Create remove command
+yargs.command({
+
+  command:'remove',
+  describe:'Remove a new note',
   handler: function () {
 
-    console.log('Adding a new note')
+    console.log('Remove a new note')
+
+  }
+
+})
+
+//Create list command
+yargs.command({
+
+  command:'list',
+  describe:'List your godang notes',
+  handler: function () {
+
+    console.log('Here is you goddam notes bub...')
+
+  }
+
+})
+
+//Create read command
+yargs.command({
+
+  command:'read',
+  describe:'Read em notes',
+  handler: function () {
+
+    console.log('REading, reading, reading...')
 
   }
 
